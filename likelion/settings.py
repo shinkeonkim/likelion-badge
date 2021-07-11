8ec4e6c88a0de8d39cee0262eb4c4cddbbb2b2ec
shinkeonkim <singun11@kookmin.ac.kr>
2021-07-12 03:07:02 +0900

    chore: heroku additional settings

--- likelion/settings.py
+++ likelion/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url, os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
