9fb47362718ee51827704731ceb363f279edf6a5
shinkeonkim <singun11@kookmin.ac.kr>
2021-07-13 21:03:17 +0900

    feat: likelion_university_badge_v1 추가, 공식 색상 및 폰트 사용

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import likelion_shield_badge
+from .views import likelion_shield_badge, likelion_university_badge_v1
 
 app_name = 'api'
 
 urlpatterns = [
   path('likelion_shield_badge', likelion_shield_badge),
+  path('likelion_university_badge/v1', likelion_university_badge_v1),
 ]
