3a4228d488146808595680cae91fdf391cf038bd
shinkeonkim <singun11@kookmin.ac.kr>
2021-07-17 16:51:31 +0900

    feat: vscode settings 추가 및 indentation 통일

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .views import likelion_shield_badge, likelion_university_badge_v1
 app_name = 'api'
 
 urlpatterns = [
-  path('likelion_shield_badge', likelion_shield_badge),
-  path('likelion_university_badge/v1', likelion_university_badge_v1),
+    path('likelion_shield_badge', likelion_shield_badge),
+    path('likelion_university_badge/v1', likelion_university_badge_v1),
 ]
